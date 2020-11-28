4802f05db6d578c387be1cb9603c6ae8ccd94365
Latteholic <cafelatte@gmail.com>
2020-11-29 00:57:47 +0900

    (delete)

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,14 +1,15 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+
   with_options presence: true do
     validates :name, length:{maximum: 35}
     validates :persons
     validates :recipe
     validates :is_public
+    validates :time, allow_blank: true
+    validates :tips, allow_blank: true
+    validates :calories, allow_blank: true
   end
 
-  with_options allow_blanc: true do
-    validates :time
-    validates :tips
-    validates :calories
-  end
+
 end
