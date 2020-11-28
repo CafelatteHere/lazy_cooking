b2f9174a9866b07e6a09d2f81d438048529bbf6e
Latteholic <cafelatte@gmail.com>
2020-11-28 15:16:38 +0900

    レシピテーブルマイグレーション

--- db/migrate/20201127150232_create_recipes.rb
+++ db/migrate/20201127150232_create_recipes.rb
@@ -1,12 +1,12 @@
 class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
-      t.references    :user, null: false, foreign_key: true
+      # t.references    :user, null: false, foreign_key: true
       t.string        :name, null: false
       t.integer       :persons, null: false
       t.date          :time
-      t.text_field    :recipe, null: false
-      t.text_field    :tips
+      t.text          :recipe, null: false
+      t.text          :tips
       t.integer       :calories
       t.boolean       :is_public
       t.timestamps
