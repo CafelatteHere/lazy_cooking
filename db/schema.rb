b7e5d3da15c25cc1400a70ce73a9fa74fa59f5e1
Latteholic <cafelatte@gmail.com>
2020-11-29 00:57:52 +0900

    Revert "(delete)"
    This reverts commit 4802f05db6d578c387be1cb9603c6ae8ccd94365.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_085658) do
+ActiveRecord::Schema.define(version: 2020_11_27_150232) do
 
   create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -20,19 +20,6 @@ ActiveRecord::Schema.define(version: 2020_11_28_085658) do
     t.text "tips"
     t.integer "calories"
     t.boolean "is_public"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "name", default: "", null: false
-    t.string "surname", default: "", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.date "birthday", null: false
-    t.integer "country_id"
-    t.text "about_me"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
