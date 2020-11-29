b6548ba6e6eb18c5b23561d1750ac2a0392b7281
Latteholic <cafelatte@gmail.com>
2020-11-29 16:41:07 +0900

    ユザーモデル記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :recipes
+
+  with_options presence: true do
+    validates :nickname, length:{maximum: 20}
+    validates :name, length: {maximum: 15}
+    validates :surname, length: {maximum: 15}
+    validates :birthday
+    validates :country_id, allow_blank: true, numericality: { other_than: 1 }
+    validates :about_me, allow_blank: true
+  end
 end
