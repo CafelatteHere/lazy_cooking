0a4fa13960af70dab4f3493f96340fa0914e3ca5
Latteholic <cafelatte@gmail.com>
2021-01-03 12:57:30 +0900

    migration実行

--- db/schema.rb
+++ db/schema.rb
@@ -12,27 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_12_15_162747) do
 
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
   create_table "calendars", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "day", null: false
     t.bigint "recipe_id", null: false
@@ -41,35 +20,16 @@ ActiveRecord::Schema.define(version: 2020_12_15_162747) do
     t.index ["recipe_id"], name: "index_calendars_on_recipe_id"
   end
 
-  create_table "ingredients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "i_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "recipe_ingredient_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "recipe_id"
-    t.bigint "ingredient_id"
-    t.string "quantity", null: false
-    t.integer "measurement_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["ingredient_id"], name: "index_recipe_ingredient_relations_on_ingredient_id"
-    t.index ["recipe_id"], name: "index_recipe_ingredient_relations_on_recipe_id"
-  end
-
   create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "portions", null: false
-    t.integer "time_count_id", null: false
+    t.integer "persons", null: false
+    t.date "time"
     t.text "recipe", null: false
     t.text "tips"
     t.integer "calories"
     t.boolean "is_public"
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -90,9 +50,5 @@ ActiveRecord::Schema.define(version: 2020_12_15_162747) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "calendars", "recipes"
-  add_foreign_key "recipe_ingredient_relations", "ingredients"
-  add_foreign_key "recipe_ingredient_relations", "recipes"
-  add_foreign_key "recipes", "users"
 end
