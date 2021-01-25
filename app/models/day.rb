aa0d008252bc4fbe1545115b7abd2c87d5948c1c
Latteholic <cafelatte@gmail.com>
2021-01-25 22:40:15 +0900

    カレンダーでの日付表示訂正

--- app/models/day.rb
+++ app/models/day.rb
@@ -3,15 +3,14 @@ class Day < ActiveHash::Base
   @day = now.day
   @i = now.wday
   @wdays = ['日', '月', '火', '水', '木', '金', '土', '日', '月', '火', '水', '木', '金', '土']
-  @wday = @wdays[@i]
 
   self.data = [
-    { id: 1, name: "今日　（#{@day}日 #{@wday[@i]}曜日）"},
-    { id: 2, name: "明日　（#{@day+1}日 #{@wday[@i+1]}曜日）"},
-    { id: 3, name: "明後日（#{@day+2}日 #{@wday[@i+2]}曜日）"},
-    { id: 4, name: "#{@day+3}日（#{@wday[@i+3]}曜日）"},
-    { id: 5, name: "#{@day+4}日（#{@wday[@i+4]}曜日）"},
-    { id: 6, name: "#{@day+5}日（#{@wday[@i+5]}曜日）"},
-    { id: 7, name: "#{@day+6}日（#{@wday[@i+6]}曜日）"}
+    { id: 1, name: "今日　（#{@day}日 #{@wdays[@i]}曜日）"},
+    { id: 2, name: "明日　（#{@day+1}日 #{@wdays[@i+1]}曜日）"},
+    { id: 3, name: "明後日（#{@day+2}日 #{@wdays[@i+2]}曜日）"},
+    { id: 4, name: "#{@day+3}日（#{@wdays[@i+3]}曜日）"},
+    { id: 5, name: "#{@day+4}日（#{@wdays[@i+4]}曜日）"},
+    { id: 6, name: "#{@day+5}日（#{@wdays[@i+5]}曜日）"},
+    { id: 7, name: "#{@day+6}日（#{@wdays[@i+6]}曜日）"}
   ]
 end
