13703f43da07caf49e497ecf66597fc0535ddf4d
Latteholic <cafelatte@gmail.com>
2021-02-06 00:11:49 +0900

    deploy.rbの記述訂正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ lock '3.15.0'
 
 set :application, 'lazy_cooking'
 
-set :repo_url,  'git@github.com:https://github.com/CafelatteHere/lazy_cooking.git'
+set :repo_url,  'git@github.com:CafelatteHere/lazy_cooking.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
