5901f3b54ae47feac8b7d4bda551894d11be1596
Latteholic <cafelatte@gmail.com>
2021-02-10 00:53:38 +0900

    measurement.rbの訂正

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,14 +1,13 @@
 class Measurement < ActiveHash::Base
   self.data = [
-    {id: 1, name: '--'},
-    {id: 2, name: 'kg'},
-    {id: 3, name: 'ml'},
-    {id: 4, name: 'g'},
-    {id: 5, name: '大さじ'},
-    {id: 6, name: '小さじ'},
-    {id: 7, name: '個'},
-    {id: 8, name: 'お好みで'},
-    {id: 9, name: '少々'},
+    {id: 1, name: 'g'},
+    {id: 2, name: 'ml'},
+    {id: 3, name: '大さじ'},
+    {id: 4, name: '小さじ'},
+    {id: 5, name: '個'},
+    {id: 6, name: '枚'},
+    {id: 7, name: 'お好みで'},
+    {id: 8, name: '少々'},
   ]
 
   include ActiveHash::Associations
