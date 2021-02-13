64eb135d2d6ffdd122292301b2046ac2aced570e
Latteholic <cafelatte@gmail.com>
2021-02-13 22:50:24 +0900

    Revert "Revert "deploy.rbの訂正""
    This reverts commit 0fa1544d13c199928b202dc48242da281e8cf396.

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rbenv_ruby, '2.6.5'
 
 
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/id_rsa.pem']
+                                  keys: ['~/.ssh/ec1ec2ec3.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
