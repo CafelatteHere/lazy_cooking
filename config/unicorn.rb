ad740fc6f0d1b6a483cf144f31fab5b2369eae93
Latteholic <cafelatte@gmail.com>
2021-02-14 17:37:16 +0900

    Revert "capistrano記述訂正"
    This reverts commit 3ed9dc367538b29ce55c450fc940d843e96fee32.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,6 +11,7 @@ listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+
 timeout 60
 
 
